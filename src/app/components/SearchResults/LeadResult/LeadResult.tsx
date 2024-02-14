4bbf8c8f7586ae67c72941b3c1ecbe6637c8e88a
Ian Jennings <ian@work-mini.local>
2024-02-14 17:39:39 -0600

    one more cleanup

--- src/app/components/SearchResults/LeadResult/LeadResult.tsx
+++ src/app/components/SearchResults/LeadResult/LeadResult.tsx
@@ -1,6 +1,6 @@
 import { StackOverflowResult } from "@/app/types/StackOverflowResult";
 import { ClickableTile, Tag, Tile } from "@carbon/react";
-import { decode } from 'html-entities';
+import { decode } from "html-entities";
 import { useEffect, useState } from "react";
 
 import "./LeadResult.scss";
@@ -10,7 +10,6 @@ interface LeadResultProps {
 }
 
 const LeadResult: React.FC<LeadResultProps> = ({ item, ...props }) => {
-
   const [key, setKey] = useState(new Date().getTime());
 
   useEffect(() => {
@@ -18,27 +17,41 @@ const LeadResult: React.FC<LeadResultProps> = ({ item, ...props }) => {
   }, [item]);
 
   let result;
-  console.log(item)
-  if (!item) { result = <Tile><p className="center">Start typing your code quandry and when you are done typing, you shall receive the best single stack overflow result I can provide</p></Tile>; }
+  console.log(item);
+  if (!item) {
+    result = (
+      <Tile>
+        <p className="center">
+          Start typing your code quandry and when you are done typing, you shall
+          receive the best single stack overflow result I can provide
+        </p>
+      </Tile>
+    );
+  }
   if (item && item.title !== "noresult") {
     result = (
       <ClickableTile key={key} id="lead-result" href={item.link}>
         <span className="tile-label">Top Result</span>
         <h3 className="lead-result-title">{decode(item.title)}</h3>
         Tags: &nbsp;
-        {item.tags ? item.tags.map(tag => (
-          <Tag key={tag} className="some-class" type="cyan" title="tag">
-            {tag}
-          </Tag>
-        )) : null}
+        {item.tags
+          ? item.tags.map((tag) => (
+            <Tag key={tag} className="some-class" type="cyan" title="tag">
+              {tag}
+            </Tag>
+          ))
+          : null}
       </ClickableTile>
     );
   } else {
-    result = <Tile><p>Nope. Try again friend.</p></Tile>;
+    result = (
+      <Tile>
+        <p>Nope. Try again friend.</p>
+      </Tile>
+    );
   }
 
   return result;
-}
+};
 
 export default LeadResult;
-
